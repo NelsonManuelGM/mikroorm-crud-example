5aba05b713e013173ca977d13988c4bfc30124c2
Nelson <43588033+NelsonManuelGM@users.noreply.github.com>
2021-09-28 08:13:44 -0400

    configuring main file to validate data

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    })
+  );
   await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
