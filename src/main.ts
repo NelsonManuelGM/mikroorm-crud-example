c2fb3e79952828545b9c29b4edb04e126e426889
Nelson <43588033+NelsonManuelGM@users.noreply.github.com>
2021-09-28 20:28:53 -0400

    adding objectIdpipe and configuration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { LoggerMiddleware } from "./middlewares/logger.middleware";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,8 @@ async function bootstrap() {
       },
     })
   );
+
+  app.use(LoggerMiddleware);
   await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
