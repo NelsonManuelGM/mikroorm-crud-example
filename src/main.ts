e8b0462ca20babf4c3e7b489dfa2d5466d9beab8
Nelson <43588033+NelsonManuelGM@users.noreply.github.com>
2021-09-29 17:58:50 -0400

    adding swagger

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { ValidationPipe } from "@nestjs/common";
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { LoggerMiddleware } from "./middlewares/logger.middleware";
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,8 +15,17 @@ async function bootstrap() {
       transformOptions: {
         enableImplicitConversion: true,
       },
-    })
+    }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('House CRUD')
+    .setVersion('1.0')
+    .addTag('house')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.use(LoggerMiddleware);
   await app.listen(process.env.SERVER_PORT);
